107ef41ca6c67ad71b0e07709a273af4faaf95e8
Federico Salinas <salinasfederico773@gmail.com>
2024-05-03 15:58:23 -0600

    Removed useless print

--- syntax/CA_TSP.py
+++ syntax/CA_TSP.py
@@ -148,5 +148,3 @@ if __name__ == "__main__":
     TSP_Capitales.FindSolution()
     print(TSP_Capitales.best_route, TSP_Capitales.best_route_dist)
     TSP_Capitales.ShowCurrentSolution()
-
-    print("Hi! :)")
